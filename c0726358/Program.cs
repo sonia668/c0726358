04b16cc79f91d062cc02b3701b807b453fb428f5
visualstudio <visualstudio@230-PC43>
2019-01-31 15:04:55 -0500

    commity

--- c0726358/Program.cs
+++ c0726358/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 
-namespace c0726358
+namespace ConsoleApp1
 {
-    class Program
+    class village
     {
+        public static int numberofvillages = 0;
+        public village nextvillage;
+        public village previouslyvillage;
+        public string villagename;
+        public bool isAstrildehere = false;
+
+        public village()
+        {
+            village.numberofvillages++;
+        }
+
+
+        class countryside
+        {
+            public village Maple;
+
+            public village Toronto;
+            public village Ajax;
+
+            public void MapInitializer()
+            {
+                Maple = new village();
+                Maple.villagename = "Maple";
+                Maple.previouslyvillage = null;
+                Maple.nextvillage = Toronto;
+                Toronto = new village();
+                Toronto.villagename = "Toronto ";
+                Toronto.previouslyvillage = Maple;
+                Toronto.nextvillage = Ajax;
+                Ajax = new village();
+                Ajax.villagename = "Ajax";
+                Ajax.previouslyvillage = Toronto;
+                Ajax.nextvillage = null;
+            }
+
+            public void LookForAstride()
+            {
+
+            }
+
+
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
     }
 }
+
